cfe60bf7bd8e36059a198ab5ca24ae608d81b3db
Semchenock <64313471+Semchenock@users.noreply.github.com>
2022-05-08 21:35:27 +0300

    Added cleanAll fixed bug with drag'n'drop cleaning

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const closeBtn = document.querySelector(".cancel.btn");
 const form = document.querySelector(".task.form");
 const dropZones = document.querySelectorAll(".block");
 const dragItems = document.querySelectorAll(".task[draggable]");
-// const taskLists = document.querySelectorAll(".tasks");
+const taskLists = document.querySelectorAll(".tasks");
 const deleteBtn = document.querySelector(".clean");
 
 let draggedItem = null;
@@ -40,13 +40,18 @@ function addTask(text) {
     addListners(newTask);
   }
 }
+function deleteAll() {
+  taskLists[3].innerHTML = "";
+}
 
 function handlerDragenterElement() {
   if (this !== draggedItem) {
+    console.log("croseed", this);
     droppedItem = this;
   }
 }
 function handlerDragleaveElement() {
+  console.log("leaved", this);
   droppedItem = null;
 }
 function addListners(dragItem) {
@@ -56,10 +61,8 @@ function addListners(dragItem) {
   dragItem.addEventListener("dragleave", handlerDragleaveElement);
 }
 function handlerDragstart() {
-  if (this.classList.contains(".task")) {
-    draggedItem = this;
-    this.classList.add("dragItem--active");
-  }
+  draggedItem = this;
+  this.classList.add("dragItem--active");
 }
 function handlerDragend() {
   draggedItem = null;
@@ -72,25 +75,32 @@ function handlerDragover(e) {
   e.preventDefault();
 }
 function handlerDrop() {
-  if (droppedItem) {
-    if (droppedItem === deleteBtn) {
-      draggedItem.remove();
-    } else if (droppedItem.parentElement === draggedItem.parentElement) {
-      const children = Array.from(draggedItem.parentElement.children);
-      const indexOfDropped = children.indexOf(droppedItem);
-      const indexOfDragged = children.indexOf(draggedItem);
-      if (indexOfDropped > indexOfDragged) {
-        insertAfter(draggedItem, droppedItem);
+  if (draggedItem) {
+    if (droppedItem) {
+      if (droppedItem === deleteBtn) {
+        draggedItem.remove();
+      } else if (droppedItem.parentElement === draggedItem.parentElement) {
+        const children = Array.from(draggedItem.parentElement.children);
+        const indexOfDropped = children.indexOf(droppedItem);
+        const indexOfDragged = children.indexOf(draggedItem);
+        if (indexOfDropped > indexOfDragged) {
+          insertAfter(draggedItem, droppedItem);
+        } else {
+          insertBefore(draggedItem, droppedItem);
+        }
       } else {
-        insertBefore(draggedItem, droppedItem);
+        insertAfter(draggedItem, droppedItem);
       }
     } else {
-      insertAfter(draggedItem, droppedItem);
+      console.log(droppedItem);
+      console.log(this);
+      if (this === deleteBtn) {
+        draggedItem.remove();
+      }
+      const zoneFlag = this.dataset.zone;
+      const taskList = document.querySelector(`.${zoneFlag}.tasks`);
+      taskList.append(draggedItem);
     }
-  } else {
-    const zoneFlag = this.dataset.zone;
-    const taskList = document.querySelector(`.${zoneFlag}.tasks`);
-    taskList.append(draggedItem);
   }
 }
 
@@ -113,4 +123,4 @@ deleteBtn.addEventListener("dragover", handlerDragover);
 deleteBtn.addEventListener("dragenter", handlerDragenterElement);
 deleteBtn.addEventListener("dragleave", handlerDragleaveElement);
 deleteBtn.addEventListener("drop", handlerDrop);
-// deleteBtn.addEventListener("click", deleteAll);
+deleteBtn.addEventListener("click", deleteAll);
